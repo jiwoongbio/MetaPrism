ef6abdfa66d52084654b887ab38ad58c09e13a7e
jiwoongbio <jiwoongbio@gmail.com>
2019-05-31 12:13:51 -0500

    first commit

--- example/example.sh
+++ example/example.sh
@@ -4,101 +4,86 @@
 perl ../MetaPrism_gene_prepare.pl
 
 # Gene-taxon annotation and abundance quantification
-#(cd P1; ./P1.sh)
-#(cd P2; ./P2.sh)
-#(cd P3; ./P3.sh)
-#(cd P4; ./P4.sh)
-#(cd P5; ./P5.sh)
-#(cd P6; ./P6.sh)
-#(cd P7; ./P7.sh)
-#(cd P8; ./P8.sh)
-#(cd P9; ./P9.sh)
-#(cd P10; ./P10.sh)
-#(cd P12; ./P12.sh)
-#(cd P19; ./P19.sh)
-#(cd P31; ./P31.sh)
-#(cd P55; ./P55.sh)
-#(cd P63; ./P63.sh)
-#(cd P78; ./P78.sh)
+(cd P10; ./P10.sh)
+(cd P14; ./P14.sh)
+(cd P23; ./P23.sh)
+(cd P25; ./P25.sh)
+(cd P34; ./P34.sh)
+(cd P39; ./P39.sh)
+(cd P8; ./P8.sh)
+(cd P16; ./P16.sh)
+(cd P22; ./P22.sh)
+(cd P24; ./P24.sh)
+(cd P30; ./P30.sh)
+(cd P32; ./P32.sh)
+(cd P42; ./P42.sh)
 
 # Compare sample groups and identify differentially-abundant genes
 perl ../MetaPrism_comparison.pl -F gene sample.group.txt \
-	P1=P1/P1.gene_taxon.region.abundance.txt \
-	P2=P2/P2.gene_taxon.region.abundance.txt \
-	P3=P3/P3.gene_taxon.region.abundance.txt \
-	P4=P4/P4.gene_taxon.region.abundance.txt \
-	P5=P5/P5.gene_taxon.region.abundance.txt \
-	P6=P6/P6.gene_taxon.region.abundance.txt \
-	P7=P7/P7.gene_taxon.region.abundance.txt \
-	P8=P8/P8.gene_taxon.region.abundance.txt \
-	P9=P9/P9.gene_taxon.region.abundance.txt \
 	P10=P10/P10.gene_taxon.region.abundance.txt \
-	P12=P12/P12.gene_taxon.region.abundance.txt \
-	P19=P19/P19.gene_taxon.region.abundance.txt \
-	P31=P31/P31.gene_taxon.region.abundance.txt \
-	P55=P55/P55.gene_taxon.region.abundance.txt \
-	P63=P63/P63.gene_taxon.region.abundance.txt \
-	P78=P78/P78.gene_taxon.region.abundance.txt \
+	P14=P14/P14.gene_taxon.region.abundance.txt \
+	P23=P23/P23.gene_taxon.region.abundance.txt \
+	P25=P25/P25.gene_taxon.region.abundance.txt \
+	P34=P34/P34.gene_taxon.region.abundance.txt \
+	P39=P39/P39.gene_taxon.region.abundance.txt \
+	P8=P8/P8.gene_taxon.region.abundance.txt \
+	P16=P16/P16.gene_taxon.region.abundance.txt \
+	P22=P22/P22.gene_taxon.region.abundance.txt \
+	P24=P24/P24.gene_taxon.region.abundance.txt \
+	P30=P30/P30.gene_taxon.region.abundance.txt \
+	P32=P32/P32.gene_taxon.region.abundance.txt \
+	P42=P42/P42.gene_taxon.region.abundance.txt \
 	> gene.comparison.txt
-
-# Prediction model accuracy
-perl ../MetaPrism_prediction.pl sample.group.txt \
-	P1=P1/P1.gene_taxon.region.abundance.txt \
-	P2=P2/P2.gene_taxon.region.abundance.txt \
-	P3=P3/P3.gene_taxon.region.abundance.txt \
-	P4=P4/P4.gene_taxon.region.abundance.txt \
-	P5=P5/P5.gene_taxon.region.abundance.txt \
-	P6=P6/P6.gene_taxon.region.abundance.txt \
-	P7=P7/P7.gene_taxon.region.abundance.txt \
-	P8=P8/P8.gene_taxon.region.abundance.txt \
-	P9=P9/P9.gene_taxon.region.abundance.txt \
-	P10=P10/P10.gene_taxon.region.abundance.txt \
-	P12=P12/P12.gene_taxon.region.abundance.txt \
-	P19=P19/P19.gene_taxon.region.abundance.txt \
-	P31=P31/P31.gene_taxon.region.abundance.txt \
-	P55=P55/P55.gene_taxon.region.abundance.txt \
-	P63=P63/P63.gene_taxon.region.abundance.txt \
-	P78=P78/P78.gene_taxon.region.abundance.txt \
-	> prediction.txt
-
-# Generate table
-perl ../MetaPrism_table.pl -F taxon_average -s \
-	P1=P1/P1.gene_taxon.region.abundance.txt \
-	P2=P2/P2.gene_taxon.region.abundance.txt \
-	P3=P3/P3.gene_taxon.region.abundance.txt \
-	P4=P4/P4.gene_taxon.region.abundance.txt \
-	P5=P5/P5.gene_taxon.region.abundance.txt \
-	P6=P6/P6.gene_taxon.region.abundance.txt \
-	P7=P7/P7.gene_taxon.region.abundance.txt \
-	P8=P8/P8.gene_taxon.region.abundance.txt \
-	P9=P9/P9.gene_taxon.region.abundance.txt \
-	P10=P10/P10.gene_taxon.region.abundance.txt \
-	P12=P12/P12.gene_taxon.region.abundance.txt \
-	P19=P19/P19.gene_taxon.region.abundance.txt \
-	P31=P31/P31.gene_taxon.region.abundance.txt \
-	P55=P55/P55.gene_taxon.region.abundance.txt \
-	P63=P63/P63.gene_taxon.region.abundance.txt \
-	P78=P78/P78.gene_taxon.region.abundance.txt \
-	> taxon.table.txt
 
 awk -F'\t' '(NR == 1 || ($4 > 1 && $5 < 0.01))' gene.comparison.txt > gene.comparison.filtered.txt
 
 # Generate heatmap
 perl ../MetaPrism_heatmap.pl -F gene -s -g gene.comparison.filtered.txt -r both \
-	P1=P1/P1.gene_taxon.region.abundance.txt \
-	P2=P2/P2.gene_taxon.region.abundance.txt \
-	P3=P3/P3.gene_taxon.region.abundance.txt \
-	P4=P4/P4.gene_taxon.region.abundance.txt \
-	P5=P5/P5.gene_taxon.region.abundance.txt \
-	P6=P6/P6.gene_taxon.region.abundance.txt \
-	P7=P7/P7.gene_taxon.region.abundance.txt \
-	P8=P8/P8.gene_taxon.region.abundance.txt \
-	P9=P9/P9.gene_taxon.region.abundance.txt \
 	P10=P10/P10.gene_taxon.region.abundance.txt \
-	P12=P12/P12.gene_taxon.region.abundance.txt \
-	P19=P19/P19.gene_taxon.region.abundance.txt \
-	P31=P31/P31.gene_taxon.region.abundance.txt \
-	P55=P55/P55.gene_taxon.region.abundance.txt \
-	P63=P63/P63.gene_taxon.region.abundance.txt \
-	P78=P78/P78.gene_taxon.region.abundance.txt \
+	P14=P14/P14.gene_taxon.region.abundance.txt \
+	P23=P23/P23.gene_taxon.region.abundance.txt \
+	P25=P25/P25.gene_taxon.region.abundance.txt \
+	P34=P34/P34.gene_taxon.region.abundance.txt \
+	P39=P39/P39.gene_taxon.region.abundance.txt \
+	P8=P8/P8.gene_taxon.region.abundance.txt \
+	P16=P16/P16.gene_taxon.region.abundance.txt \
+	P22=P22/P22.gene_taxon.region.abundance.txt \
+	P24=P24/P24.gene_taxon.region.abundance.txt \
+	P30=P30/P30.gene_taxon.region.abundance.txt \
+	P32=P32/P32.gene_taxon.region.abundance.txt \
+	P42=P42/P42.gene_taxon.region.abundance.txt \
 	> gene.heatmap.html
+
+# Generate table
+perl ../MetaPrism_table.pl -F taxon_average -s \
+	P10=P10/P10.gene_taxon.region.abundance.txt \
+	P14=P14/P14.gene_taxon.region.abundance.txt \
+	P23=P23/P23.gene_taxon.region.abundance.txt \
+	P25=P25/P25.gene_taxon.region.abundance.txt \
+	P34=P34/P34.gene_taxon.region.abundance.txt \
+	P39=P39/P39.gene_taxon.region.abundance.txt \
+	P8=P8/P8.gene_taxon.region.abundance.txt \
+	P16=P16/P16.gene_taxon.region.abundance.txt \
+	P22=P22/P22.gene_taxon.region.abundance.txt \
+	P24=P24/P24.gene_taxon.region.abundance.txt \
+	P30=P30/P30.gene_taxon.region.abundance.txt \
+	P32=P32/P32.gene_taxon.region.abundance.txt \
+	P42=P42/P42.gene_taxon.region.abundance.txt \
+	> taxon.table.txt
+
+# Prediction model accuracy
+perl ../MetaPrism_prediction.pl sample.group.txt \
+	P10=P10/P10.gene_taxon.region.abundance.txt \
+	P14=P14/P14.gene_taxon.region.abundance.txt \
+	P23=P23/P23.gene_taxon.region.abundance.txt \
+	P25=P25/P25.gene_taxon.region.abundance.txt \
+	P34=P34/P34.gene_taxon.region.abundance.txt \
+	P39=P39/P39.gene_taxon.region.abundance.txt \
+	P8=P8/P8.gene_taxon.region.abundance.txt \
+	P16=P16/P16.gene_taxon.region.abundance.txt \
+	P22=P22/P22.gene_taxon.region.abundance.txt \
+	P24=P24/P24.gene_taxon.region.abundance.txt \
+	P30=P30/P30.gene_taxon.region.abundance.txt \
+	P32=P32/P32.gene_taxon.region.abundance.txt \
+	P42=P42/P42.gene_taxon.region.abundance.txt \
+	> prediction.txt
