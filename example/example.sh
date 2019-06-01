581c7dc45d6aa2f28a3bc9e14efcaa42171814c1
zhanxw <zhanxw@users.noreply.github.com>
2019-05-31 21:36:27 -0500

    Update example.sh

--- example/example.sh
+++ example/example.sh
@@ -12,7 +12,6 @@ perl ../MetaPrism_gene_prepare.pl
 (cd P39; ./P39.sh)
 (cd P8; ./P8.sh)
 (cd P16; ./P16.sh)
-(cd P22; ./P22.sh)
 (cd P24; ./P24.sh)
 (cd P30; ./P30.sh)
 (cd P32; ./P32.sh)
@@ -28,7 +27,6 @@ perl ../MetaPrism_comparison.pl -F gene sample.group.txt \
 	P39=P39/P39.gene_taxon.region.abundance.txt \
 	P8=P8/P8.gene_taxon.region.abundance.txt \
 	P16=P16/P16.gene_taxon.region.abundance.txt \
-	P22=P22/P22.gene_taxon.region.abundance.txt \
 	P24=P24/P24.gene_taxon.region.abundance.txt \
 	P30=P30/P30.gene_taxon.region.abundance.txt \
 	P32=P32/P32.gene_taxon.region.abundance.txt \
@@ -47,7 +45,6 @@ perl ../MetaPrism_heatmap.pl -F gene -s -g gene.comparison.filtered.txt -r both 
 	P39=P39/P39.gene_taxon.region.abundance.txt \
 	P8=P8/P8.gene_taxon.region.abundance.txt \
 	P16=P16/P16.gene_taxon.region.abundance.txt \
-	P22=P22/P22.gene_taxon.region.abundance.txt \
 	P24=P24/P24.gene_taxon.region.abundance.txt \
 	P30=P30/P30.gene_taxon.region.abundance.txt \
 	P32=P32/P32.gene_taxon.region.abundance.txt \
@@ -64,7 +61,6 @@ perl ../MetaPrism_table.pl -F taxon_average -s \
 	P39=P39/P39.gene_taxon.region.abundance.txt \
 	P8=P8/P8.gene_taxon.region.abundance.txt \
 	P16=P16/P16.gene_taxon.region.abundance.txt \
-	P22=P22/P22.gene_taxon.region.abundance.txt \
 	P24=P24/P24.gene_taxon.region.abundance.txt \
 	P30=P30/P30.gene_taxon.region.abundance.txt \
 	P32=P32/P32.gene_taxon.region.abundance.txt \
@@ -81,7 +77,6 @@ perl ../MetaPrism_prediction.pl sample.group.txt \
 	P39=P39/P39.gene_taxon.region.abundance.txt \
 	P8=P8/P8.gene_taxon.region.abundance.txt \
 	P16=P16/P16.gene_taxon.region.abundance.txt \
-	P22=P22/P22.gene_taxon.region.abundance.txt \
 	P24=P24/P24.gene_taxon.region.abundance.txt \
 	P30=P30/P30.gene_taxon.region.abundance.txt \
 	P32=P32/P32.gene_taxon.region.abundance.txt \
